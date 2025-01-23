23ff3ab8a0a75d4e60af3bbec84d30f26ca067e3
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-01-23 09:13:59 +0100

    Create ex03.js

---
+++ ex03.js
@@ -0,0 +1,9 @@
+function estNegatif(nombre) {
+	if (nombre < 0) {
+		return "true";
+	}
+	return "false";
+}
+
+console.log(estNegatif(-3)); // Résultat attendu : true
+console.log(estNegatif(5)); // Résultat attendu : false
