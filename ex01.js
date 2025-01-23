7a51c94b2429ba9d37ddb1e4f1b0aed5a0f520d7
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-01-23 09:10:45 +0100

    Create ex01.js

---
+++ ex01.js
@@ -0,0 +1,9 @@
+function isString(myString) {
+	if (typeof myString === "string") {
+		return "true";
+	}
+	return "false";
+}
+
+console.log(isString("Marc")); // Résultat attendu : true
+console.log(isString(2)); // Résultat attendu : false
